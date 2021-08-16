b6f56f3f87e117e42b83c16a94a20778c4d3c9a0
juanc <jcduenast@unal.edu.co>
2021-08-16 16:33:40 +0200

    Data adquisition

--- get_training_data.py
+++ get_training_data.py
@@ -24,17 +24,25 @@ capture = False
 data_img = []
 data_input = []
 
+### store images as png
+save_png = True
+frame_counter = 0
+
 # Print operation info
 print("Press 'G' to start recording.\nPress 'H' to stop.\nPress 'J' to end the program ")
 
 while running:
     key_pressed = key_check()
     if capture:
-        frame = grab_screen([0, 30, 1024, 768])
+        frame = grab_screen([560, 240, 800, 600]) #grab_screen([0, 30, 1024, 768])
         frame_gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-        frame_canny = cv2.Canny(frame_gray, 100, 200)
+        # frame_canny = cv2.Canny(frame_gray, 100, 200)
         data_img.append(frame_gray)
         data_input.append(key_pressed)
+        if save_png:
+            cv2.imwrite(training_data_path+'/data_{:0>4}/f_{:0>4}.png'.format(data_id, frame_counter), frame_gray)
+            frame_counter += 1
+
         if 'H' in key_pressed:
             capture = False
             print('Capture OFF')
@@ -46,9 +54,23 @@ while running:
             print('data_{:0>4} saved'.format(data_id))
     else:
         if 'G' in key_pressed:
+            if save_png:
+                while os.path.isdir('../bro_falls_AI_data/training/data_{:0>4}'.format(data_id)):
+                    data_id += 1
+                os.makedirs('../bro_falls_AI_data/training/data_{:0>4}'.format(data_id))
             capture = True
+            frame_counter = 0
             print('Capture ON')
     if 'J' in key_pressed:
+        if capture:
+            capture = False
+            print('Capture OFF')
+            while os.path.exists(training_data_path + 'data_{:0>4}_input.pickle'.format(data_id)):
+                data_id += 1
+            with open(training_data_path + 'data_{:0>4}_input.pickle'.format(data_id), 'wb') as f:
+                pickle.dump(data_input, f)
+            np.save(training_data_path + 'data_{:0>4}_img'.format(data_id), np.array(data_img))
+            print('data_{:0>4} saved'.format(data_id))
         running = False
 
 # with open(training_data_path+'data_{:0>4}.pickle'.format(data_id), 'wb') as f:
